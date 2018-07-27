459a349fabffe1dfb2c079885b5607f28a5f49d2
Nvishnu19 <41769746+Nvishnu19@users.noreply.github.com>
2018-07-27 14:18:22 +0530

    Create Largenumber.java

---
+++ Largenumber.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+       int i=sc.nextInt();
+		int k=100;
+		double s;
+		s=i(i+1)/2;
+		for(i=0;i<=k;i++)
+		{
+			System.out.println("sum of natural numbers:"+s);
+		}
+	
+	}
+}
