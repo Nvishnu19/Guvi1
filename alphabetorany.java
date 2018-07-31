76f3256dec6e796fe4ed5f1f2e827eb686cb0eb5
Nvishnu19 <41769746+Nvishnu19@users.noreply.github.com>
2018-07-31 11:26:01 +0530

    Update alphabetorany.java

--- alphabetorany.java
+++ alphabetorany.java
@@ -5,21 +5,20 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class alphabetorany
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter a character");
 		char ch=sc.next().charAt(0);
 		if(ch>='a'&&ch<='z' || ch>='A'&&ch<='Z')
 		{
-			System.out.println("alphabet");
+			System.out.println("Alphabet");
 			
 		}
 		else
 		{
-			System.out.println("Invalid");
+			System.out.println("No");
 		}
 	}
 }
